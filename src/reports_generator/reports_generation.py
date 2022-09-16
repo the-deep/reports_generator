880c12b0287e6180207b002f3010e85ecb589457
sfekih <selim.fekih@epfl.ch>
2022-09-16 15:59:57 +0200

    fix typo in input text assertions

--- src/reports_generator/reports_generation.py
+++ src/reports_generator/reports_generation.py
@@ -209,11 +209,11 @@ class ReportsGenerator:
 
         n_words = get_n_words(entries_as_str) + 1
         assert (
-            n_words < 20
+            n_words > 20
         ), f"The minimum number of words in the input is 20 but yours is shorter ({n_words}), please provide a longer input text."
         assert (
             len(sent_tokenize(entries)) > 1
-        ), "The minimum number of sentences must be at least 2."
+        ), "The minimum number of input sentences must be at least 2."
 
         n_raw_text_words = get_n_words(entries_as_str)
         if n_raw_text_words < max_summary_length:
