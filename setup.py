6e3a2c0465070ae92bf6cb0e20c6611ff9d9ff1e
sfekih <selim.fekih@epfl.ch>
2023-03-08 10:56:26 +0100

    add torch cpu and sentencepiece libs to requirements

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ setup(
         "networkx>=2.0",
         # "nltk>=3.6.7",
         "transformers>=4.9.2",
-        # "sentencepiece",
+        "torch@http://download.pytorch.org/whl/cpu/torch-1.9.1%2Bcpu-cp38-cp38-linux_x86_64.whl"
+        "sentencepiece",
     ],
     # *strongly* suggested for sharing
     version="0.2",
